80be8d5595c656a0e3cbb44c7b937b085bbc382d
Alexander Pruss <arpruss@gmail.com>
2026-05-03 22:32:01 -0500

    newline

--- main.c
+++ main.c
@@ -7,99 +7,18 @@
 #define WIDTH 80
 #define MEMORY_SIZE (800*1024)
 
-#define MAX_PICK_FILES 144
-
 char memory[MEMORY_SIZE];
 FILE* myFile=NULL;
 
-static uint16_t pickFileList[MAX_PICK_FILES];
-static uint16_t numPickFiles;
-
-#define WRITE_OVERLAY_GRAY       0b110000000001
-#define WRITE_OVERLAY_FOREGROUND 0b101000000001
-#define WRITE_OVERLAY_BACKGROUND 0b100000000001
-#define WRITE_OVERLAY_ERASE 	 0b111000000001
-
-#define WIN_X1 10
-#define WIN_X2 (WIDTH-WIN_X1)
-#define WIN_Y1 3
-#define WIN_Y2 (28-WIN_Y1)
-
-static uint16_t savedX;
-static uint16_t savedY;
-static uint16_t savedFore;
-static uint16_t savedBack;
-
-static char* pickFileNamer(unsigned short i) {
-	ROMDirEntry_t* dp = getROMDirEntry(i);
-    if (dp == NULL)
-        return "";
-	static char name[MAX_FILENAME_LENGTH+1];
-    unpadFilename(name, dp->name);
-    return name;
-}
-
-static int compareFiles(const void* p1, const void* p2) {
-    char* n1 = (char*)getROMDirEntry(*(uint16_t*)p1)->name;
-    char* n2 = (char*)getROMDirEntry(*(uint16_t*)p2)->name;
-    return strncasecmp(n1, n2, MAX_FILENAME_LENGTH);
-}
-
-static unsigned short pickFileLoader(void) {
-    if (refreshDir()<0)
-        return 0;
-    
-	ROMDirEntry_t* dp;
-	int i = 0;
-	numPickFiles = 0;
-	while (NULL != (dp=getROMDirEntry(i)) && numPickFiles < MAX_PICK_FILES) {
-        if (dp->type == 1) {
-            pickFileList[numPickFiles++] = i;
-        }
-		i++;
-	}
-    
-    if (numPickFiles > 0) {
-        qsort(pickFileList, numPickFiles, sizeof(uint16_t), compareFiles);
-    }
-    
-	return numPickFiles;
-}
-
-void hp_set_window(void) {
-	savedFore = getTextForeground();
-	savedBack = getTextBackground();
-	savedX = getTextX();
-	savedY = getTextY();
-	uint16_t h = getFontHeight();
-	*SCREEN_MEMORY_CONTROL = WRITE_OVERLAY_GRAY;
-	frameRectangle(WIN_X1*FONT_WIDTH,WIN_Y1*h,WIN_X2*FONT_WIDTH,WIN_Y2*h,8);
-	*SCREEN_MEMORY_CONTROL = WRITE_OVERLAY_BACKGROUND;
-	fillRectangle(WIN_X1*FONT_WIDTH,WIN_Y1*h,WIN_X2*FONT_WIDTH,WIN_Y2*h);
-	setTextWindow(WIN_X1,WIN_Y1,WIN_X2,WIN_Y2);
-	setTextColors(WRITE_OVERLAY_FOREGROUND,WRITE_OVERLAY_BACKGROUND);
-	setTextXY(0,0);
-}
-
-void hp_clear_window() {
-	uint16_t h = getFontHeight();
-	*SCREEN_MEMORY_CONTROL = WRITE_OVERLAY_ERASE;
-	fillRectangle(WIN_X1*FONT_WIDTH-8,WIN_Y1*h-8,WIN_X2*FONT_WIDTH+8,WIN_Y2*h+8);
-	setTextWindow(0,0,0,0);
-	setTextColors(savedFore,savedBack);
-	setTextXY(savedX,savedY);
-}
-
 uint8_t openReadFile(const char* n) {
+    char name[MAX_FILENAME_LENGTH+1];
     if (myFile != NULL)
         fclose(myFile);
     if (n == NULL) {
-        hp_set_window();
-        short i = hpChooser(1, 1, WIN_X2-WIN_X1-2, WIN_Y2-WIN_Y1-2, 2, 10, pickFileLoader, pickFileNamer, CHOOSER_DISK_BASED);
-        hp_clear_window();
-        if (i < 0)
+        if (quickPickFile(1, name)) 
+            n = name;
+        else
             return 0;
-        n = pickFileNamer(i);
     }
     myFile = fopen(n, "r");
     if (myFile == NULL) {
@@ -180,9 +99,9 @@ char __attribute__((noinline)) readCharacter(void) {
     
     InputEvent_t e;
     if (getInputEvent(&e) && e.type == INPUT_KEY) {
-        if (e.data.key.nativeKey == KEY_STOP)
+        if (e.data.key.nativeKey == HP_KEY_STOP)
             reload();
-        return e.data.key.character;
+        return e.data.key.character == '\n' ? '\r' : e.data.key.character;
     }
     else
         return 0;
